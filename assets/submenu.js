fcc5fae8aa84271419e1ac7c9946123035ce9c2b
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2014-12-07 10:47:28 +0100

    Removed auto-opening of submenus -- they auto-closed too often. Now the user has to click them instead.

--- assets/submenu.js
+++ assets/submenu.js
@@ -7,8 +7,7 @@ $(function() {
 
 function init_toggleable_menus() {
     $(".page-container").on("click", hide_toggleable_menus);
-    $(".toggleable_menu").parent()
-        .on("click mouseenter mouseleave", toggle_submenu);
+    $(".toggleable_menu").parent().on("click", toggle_submenu);
     hide_toggleable_menus();
 }
 
@@ -21,14 +20,10 @@ var current_menu_timer = null;
 function toggle_submenu(event) {
     event.stopImmediatePropagation();
     var menu = $(this).children(".toggleable_menu");
-    if (event.type == "mouseleave") {
-        menu.hide();
-    } else {
-        if (current_menu_timer) {
-            clearTimeout(current_menu_timer);
-            current_menu_timer = null;
-        }
-        hide_toggleable_menus();
+    var menu_is_hidden = menu.is(":hidden");
+    hide_toggleable_menus();
+    if (menu_is_hidden) {
+        if (current_menu_timer) clearTimeout(current_menu_timer);
         current_menu_timer = setTimeout(hide_toggleable_menus, menu_timeout);
         menu.show();
     }
