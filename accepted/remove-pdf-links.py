91791c3c060cc6372d8d59544785bf4aa7a60ae9
Anoop Sarkar <anoop+github@cs.sfu.ca>
2015-04-23 00:16:42 -0700

    moved md to html

--- accepted/remove-pdf-links.py
+++ accepted/remove-pdf-links.py
@@ -5,7 +5,7 @@ title: NAACL-HLT 2015 Main Program Schedule
 """
 ispaper = re.compile('a href=\"\d+.pdf\"')
 iscommented = re.compile('<\!--')
-print header
+#print header
 with open('accepted.html', 'r') as f:
     for line in f:
         line = line.strip()
